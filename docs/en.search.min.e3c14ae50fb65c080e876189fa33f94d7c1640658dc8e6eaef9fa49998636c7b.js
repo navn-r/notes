d7f319b79d56b7fdfda134d17531f58ffe74080b
Navinn Ravindaran <home@Navinns-MacBook-Pro.local>
2020-07-07 15:05:31 -0400

    hugo rebuilding bad grrr

--- docs/en.search.min.e3c14ae50fb65c080e876189fa33f94d7c1640658dc8e6eaef9fa49998636c7b.js
+++ docs/en.search.min.e3c14ae50fb65c080e876189fa33f94d7c1640658dc8e6eaef9fa49998636c7b.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/notes/flexsearch.min.js');loadScript('/notes/en.search-data.min.14a8cc4bbdfab3d07bd49b6abe51361b90d0347c636cad2c0851f0c25bba462e.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/notes/flexsearch.min.js');loadScript('/notes/en.search-data.min.da0585591665429b49f7ffd5bb848d948a844b4c1f249f0b963fc9dff66077f5.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
