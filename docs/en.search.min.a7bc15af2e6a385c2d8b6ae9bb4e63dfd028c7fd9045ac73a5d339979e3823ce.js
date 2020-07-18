16e3a7d05129f9ba406352e15387ada0fa62f6cf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-18 20:36:35 +0000

    Deploy navn-r/notes to github.com/navn-r/notes.git:master

--- docs/en.search.min.a7bc15af2e6a385c2d8b6ae9bb4e63dfd028c7fd9045ac73a5d339979e3823ce.js
+++ docs/en.search.min.a7bc15af2e6a385c2d8b6ae9bb4e63dfd028c7fd9045ac73a5d339979e3823ce.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/notes/flexsearch.min.js');loadScript('/notes/en.search-data.min.62b9c624529dc8f49c98c54840367d79b713d475afa8d2a76612cab2ead39bdc.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/notes/flexsearch.min.js');loadScript('/notes/en.search-data.min.d97f3b7b73632b59e174e740ff996d1df075eaac7e272bf812c4bb4d12320b9b.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
